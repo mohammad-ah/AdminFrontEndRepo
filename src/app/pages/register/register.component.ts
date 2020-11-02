30277e4c0f8c216d7c599f4ae633d613e3007cbb
Mohammad <mohamad.ma09@gmail.com>
2020-11-01 18:35:24 -0700

    add register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  privacy;
+  error;
+
+	form: FormGroup = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+	  email: new FormControl(),
+  });
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
   }
 
+  register() {
+    console.log(this.form.value);
+    console.log(this.privacy);
+    if (this.privacy) {
+      this.http.post('http://localhost:8094/signup', this.form.value).subscribe((data) => {
+        console.log(data);
+        this.router.navigateByUrl('/dashboard');
+      }, error => {
+        this.error = error;
+      });
+    }
+    this.router.navigateByUrl('/dashboard');
+  }
+
+  checkPrivacy(value: string) {
+    this.privacy = value;
+  }
 }
